978bc162c10cbf8a2905cc3572ca91feedabaad5
helpfulrobot <chris@silverstripe.com>
2016-01-01 02:38:41 +1300

    Converted to PSR-2

--- code/StarRating.php
+++ code/StarRating.php
@@ -4,15 +4,16 @@
  * @author Clint Landrum <clint@adaircreative.com>
  * @date 04.21.2014
  */
-class StarRating extends DataObject {
-	
-	private static $db = array(
-		'StarRatingCategory' => 'Varchar(255)',
-		'Rating' => 'Int',
-		'MaxRating' => 'Int'
-	);
+class StarRating extends DataObject
+{
+    
+    private static $db = array(
+        'StarRatingCategory' => 'Varchar(255)',
+        'Rating' => 'Int',
+        'MaxRating' => 'Int'
+    );
 
-	private static $has_one = array(
-		'ProductReview' => 'ProductReview'
-	);
+    private static $has_one = array(
+        'ProductReview' => 'ProductReview'
+    );
 }
